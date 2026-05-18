5a2e8e2a1f43dfb97a53ab834c2ae2ee30d35ea5
Nandan Bhandary <nandanbhandary24@gmail.com>
2026-05-18 12:39:37 +0530

    Math module done

---
+++ basics/learn_math.py
@@ -0,0 +1,27 @@
+import sys
+import math
+
+sys.stdin = open("input.txt", "r")
+sys.stdout = open("output.txt", "w")
+
+
+print(math.pi)
+print(math.e)
+print(math.pow(2, 3))
+print(math.pow(2, -3))
+print(math.sqrt(16))
+print(math.factorial(5))
+print(math.gcd(10, 5))
+print(math.lcm(10, 11))
+print(math.log(2))
+print(math.log2(2))
+print(math.log10(2))
+print(math.sin(90))
+print(math.cos(90))
+print(math.tan(90))
+print(math.degrees(90))
+print(math.radians(5))
+print(math.ceil(7.6))
+print(math.floor(7.9))
+print(math.isfinite(10))
+print(math.isinf(float('inf')))
