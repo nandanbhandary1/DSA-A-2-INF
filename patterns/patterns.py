d30ed4cfcb77270a78657e91d5d346f981d41344
Nandan Bhandary <nandanbhandary24@gmail.com>
2026-05-27 10:42:33 +0530

    2nd

--- patterns/patterns.py
+++ patterns/patterns.py
@@ -24,3 +24,32 @@ class Solution:
 
 if __name__ == "__main__":
     Solution().main()
+
+
+
+class Solution:
+    
+    # Function to print pattern2
+    def pattern2(self, n):
+        
+        # Outer loop will run for rows.
+        for i in range(n):
+            
+            # Inner loop will run for columns.
+            for j in range(i+1):
+                print("*", end="")
+                
+            """ As soon as n stars are printed, move
+            to the next row and give a line break."""
+            print()
+
+    def main(self):
+        N = 5
+
+        # Create an instance of the Solution class
+        sol = Solution()
+
+        sol.pattern2(N)
+
+if __name__ == "__main__":
+    Solution().main()
