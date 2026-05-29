3e92a405dc041ab48dfe6d243faa02eaf3d9e056
Nandan Bhandary <nandanbhandary24@gmail.com>
2026-05-29 09:11:41 +0530

    7

--- patterns/patterns.py
+++ patterns/patterns.py
@@ -166,3 +166,37 @@ class Solution:
 
 if __name__ == "__main__":
     Solution().main()
+
+
+
+
+class Solution:
+    
+    # Function to print pattern7
+    def pattern7(self, n):
+        
+        # Outer loop will run for rows.
+        for i in range(0,n):
+            
+            #This loop will print the spaces
+            for j in range(0, (n-i-1)):
+                print(" ",end="")
+            
+            # This loop will print asterisk.
+            for j in range(0,2*i+1):
+                print("*", end="")
+
+            """ As soon as n stars are printed, move
+            to the next row and give a line break."""
+            print()
+
+    def main(self):
+        N = 5
+
+        # Create an instance of the Solution class
+        sol = Solution()
+
+        sol.pattern7(N)
+
+if __name__ == "__main__":
+    Solution().main()
