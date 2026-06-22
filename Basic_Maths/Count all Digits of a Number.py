658fad7a82a538cd9af73383576b351579a46578
Nandan Bhandary <nandanbhandary24@gmail.com>
2026-06-22 16:25:09 +0530

    Solution 2 added

--- Basic_Maths/Count all Digits of a Number.py
+++ Basic_Maths/Count all Digits of a Number.py
@@ -25,3 +25,27 @@ sol = Solution()
 # Function call to get count of digits in n
 ans = sol.countDigit(n)
 print(f"The count of digits in the given number is: {ans}")
+
+
+import math
+
+class Solution:
+    # Function to count the
+    # number of odd digits in N
+    def countDigit(self, n):
+        # Edge case
+        if n == 0:
+            return 1
+            
+        count = int(math.log10(n) + 1)
+        return count
+
+# Input number
+n = 6678
+
+# Creating an instance of Solution class
+sol = Solution()
+
+# Function call to get count of digits in n
+ans = sol.countDigit(n)
+print("The count of digits in the given number is:", ans)
