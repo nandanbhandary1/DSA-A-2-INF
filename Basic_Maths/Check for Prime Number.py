470787675c136cfd98f32394c6c120713b82c6d2
Nandan Bhandary <nandanbhandary24@gmail.com>
2026-07-03 21:19:00 +0530

    optimal added

--- Basic_Maths/Check for Prime Number.py
+++ Basic_Maths/Check for Prime Number.py
@@ -31,3 +31,40 @@ if __name__ == "__main__":
         print(f"{n} is a prime number.")
     else:
         print(f"{n} is not a prime number.")
+
+
+
+class Solution:
+    # Function to find whether the
+    # number is prime or not
+    def isPrime(self, n):
+        # Edge case
+        if n < 2:
+            return False
+
+        # Loop from 2 to √n
+        i = 2
+        while i * i <= n:
+            # Check if i is a divisor
+            if n % i == 0:
+                return False
+            i += 1
+
+        # Return true as the number is prime
+        return True
+
+if __name__ == "__main__":
+    n = 5
+
+    # Creating an instance of 
+    # Solution class
+    sol = Solution()
+
+    # Function call to find whether the
+    # given number is prime or not
+    ans = sol.isPrime(n)
+
+    if ans:
+        print(f"{n} is a prime number.")
+    else:
+        print(f"{n} is not a prime number.")
