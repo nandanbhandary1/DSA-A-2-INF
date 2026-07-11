9fd246bc1a2cc78fd6e3cb75b5a09a32480562e2
Nandan Bhandary <nandanbhandary24@gmail.com>
2026-07-11 23:58:15 +0530

    Optimal Code added

--- Basic_Arrays/Reverse an array.py
+++ Basic_Arrays/Reverse an array.py
@@ -34,3 +34,42 @@ if __name__ == "__main__":
     solution.reverse(arr, n) 
     print("Reversed array: ", end="")
     printArray(arr, n)
+
+
+
+
+class Solution:
+    # Function to reverse array using two pointers
+    def reverse(self, arr, n):
+        p1 = 0
+        p2 = n - 1
+        # Swap elements pointed by p1 and 
+        # p2 until they meet in the middle
+        while p1 < p2:
+            tmp = arr[p1]
+            arr[p1] = arr[p2]
+            arr[p2] = tmp
+            p1 += 1
+            p2 -= 1
+        # Return
+        return
+ 
+# Function to print array
+def printArray(arr, n):
+    for i in range(n):
+        print(arr[i], end=" ")
+    print()
+ 
+if __name__ == "__main__":
+    n = 5
+    arr = [5, 4, 3, 2, 1]
+    
+    # Creating instance of Solution class
+    solution = Solution()
+    print("Original array: ", end="")
+    printArray(arr, n)
+    
+    # Function call to reverse the array 
+    solution.reverse(arr, n) 
+    print("Reversed array: ", end="")
+    printArray(arr, n)
