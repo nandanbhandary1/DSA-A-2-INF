26a9758e4bfa574cf4dd0fe1f567df9a362f9c20
Nandan Bhandary <nandanbhandary24@gmail.com>
2026-07-13 22:34:21 +0530

    Optimal Solution added

--- Basic_Hashing/Highest Occurring Element in an Array.py
+++ Basic_Hashing/Highest Occurring Element in an Array.py
@@ -54,3 +54,49 @@ if __name__ == "__main__":
     ans = sol.mostFrequentElement(nums)
     
     print("The highest occurring element in the array is:", ans)
+    
+    
+class Solution:
+    # Function to get the highest
+    # occurring element in array n
+    def mostFrequentElement(self, nums):
+        # Variable to store maximum frequency
+        maxFreq = 0
+        
+        # Variable to store element
+        # with maximum frequency
+        maxEle = 0
+        
+        # HashMap
+        mpp = {}
+        
+        # Iterating on the array
+        for num in nums:
+            # Updating hashmap
+            if num in mpp:
+                mpp[num] += 1
+            else:
+                mpp[num] = 1
+        
+        # Iterate on the map
+        for ele, freq in mpp.items():
+            if freq > maxFreq:
+                maxFreq = freq
+                maxEle = ele
+            elif freq == maxFreq:
+                maxEle = min(maxEle, ele)
+        
+        # Return the result
+        return maxEle
+
+# Input array
+nums = [4, 4, 5, 5, 6]
+
+# Creating an instance of Solution class
+sol = Solution()
+
+# Function call to get the highest
+# occurring element in array n
+ans = sol.mostFrequentElement(nums)
+
+print(f"The highest occurring element in the array is: {ans}")
