c0ab9aa6432f02d4c88c89ec06c02ed840906518
Nandan Bhandary <nandanbhandary24@gmail.com>
2026-07-15 23:52:03 +0530

    optimal solution added

--- Basic_Hashing/Sum of Highest and Lowest Frequency.py
+++ Basic_Hashing/Sum of Highest and Lowest Frequency.py
@@ -50,3 +50,53 @@ and lowest frequency in array """
 ans = sol.sumHighestAndLowestFrequency(nums)
 
 print("The sum of highest and lowest frequency in the array is:", ans)
+
+
+
+class Solution:
+    # Function to get the sum of highest
+    # and lowest frequency in array
+    def sumHighestAndLowestFrequency(self, nums):
+        
+        # Variable to store the size of array
+        n = len(nums)
+        
+        # Variable to store maximum 
+        # and minimum frequency
+        maxFreq = 0
+        minFreq = n
+        
+        # HashMap
+        mpp = {}
+        
+        # Iterating on the array
+        for num in nums:
+            # Updating hashmap
+            if num in mpp:
+                mpp[num] += 1
+            else:
+                mpp[num] = 1
+                
+        # Iterate on the map
+        for freq in mpp.values():
+            # Update maximum and 
+            # minimum frequencies
+            maxFreq = max(maxFreq, freq)
+            minFreq = min(minFreq, freq)
+            
+        # Return the required sum
+        return maxFreq + minFreq
+
+# Test the solution
+if __name__ == "__main__":
+    nums = [1, 2, 2, 3, 3, 3]
+    
+    # Creating an instance of 
+    # Solution class
+    sol = Solution()
+    
+    # Function call to get the sum of highest
+    # and lowest frequency in array
+    ans = sol.sumHighestAndLowestFrequency(nums)
+    
+    print(f"The sum of highest and lowest frequency in the array is: {ans}")
